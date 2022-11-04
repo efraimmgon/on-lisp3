50bbfdb4373cd9dfc84bb55dae5e49c75765d859
efraimmgon <efraimmaugusto@gmail.com>
2022-11-03 20:58:02 -0400

    a model of macros

--- src/on_lisp/chap07.clj
+++ src/on_lisp/chap07.clj
@@ -1,6 +1,7 @@
 (ns on-lisp.chap07
   (:require
-   clojure.pprint))
+   clojure.pprint
+   [on-lisp.utils :as utils]))
 
 ;;; ----------------------------------------------------------------------------
 ;;; 7
@@ -47,3 +48,25 @@
 #_(mac (when-bind [x (range 5)]
                   (prn x)))
 
+;;; ----------------------------------------------------------------------------
+;;; 7.6 A Model of Macros
+
+(defmacro our-expander [name]
+  `(:expander (meta #'~name)))
+
+(defmacro our-defmacro [name params & body]
+  (let [g (gensym)]
+    `(do
+       (alter-meta! #'~name
+                    assoc :expander
+                    (fn [~g]
+                      (let [~params (rest ~g)]
+                        ~@body))))))
+
+; Note: this throws for some reason
+#_(defn our-macroexpand-1
+    [expr]
+    (if (and (utils/consp expr)
+             (our-expander (first expr)))
+      ((our-expander (first expr)) expr)
+      expr))
