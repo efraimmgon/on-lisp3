19fd9436c1d742ddfa77674b4ed1d51c60a64b72
efraimmgon <efraimmaugusto@gmail.com>
2022-11-05 11:39:35 -0400

    macros from functions

--- src/on_lisp/chap07.clj
+++ src/on_lisp/chap07.clj
@@ -105,3 +105,56 @@
     `(if (first args)
        (our-and ~@(rest args)))))
 
+;;; ----------------------------------------------------------------------------
+;;; 7.10 Macros from Functions
+
+;; The difficulty of translating a function into a macro depends on a number 
+;; of properties of the function. The easiest class to translate are the functions 
+;; which
+
+;; 1. Have a body consisting of a single expression.
+;; 2. Have a parameter list consisting only of parameter names.
+;; 3. Create no new variables (except the parameters).
+;; 4. Are not recursive (not part of a mutually recursive group).
+;; 5. Have no parameter which occurs more than once in the body.
+;; 6. Have no parameter whose value is used before that of another parameter 
+;; occurring before it in the parameter list.
+;; 7. Contain no free variables.
+
+;; Where a function definition meets all the conditions above, you can easily 
+;; transform it into an equivalent macro definition. Simply put a backquote in 
+;; front of the body and a comma in front of each symbol which occurs in the 
+;; parameter list:
+(defmacro our-second [x] `(first (rest ~x)))
+
+;; The technique changes slightly when the body has more than one expression, 
+;; because a macro must expand into a single expression. So if condition 1 
+;; does't hold, you have to add a progn.
+(defmacro noisy-second [x]
+  `(do (println "Someone is taking a cadr!")
+       (first (rest ~x))))
+
+;; When the function doesn't meet condition 2 because it has an &rest or 
+;; &body parameter, the rules are the same, except that the parameter, 
+;; instead of simply having a comma before it, must be spliced into a call 
+;; to list.
+
+(defn sum [& args]
+  (apply + args))
+
+(defmacro sum [& args]
+  `(+ ~@args))
+
+;; When condition 3 doesn't hold - when new variables are created within the 
+;; function body - the rule about the insertion of commas must be modified. 
+;; Instead of putting commas before all symbols in the parameter list, we only 
+;; put them before those which will refer to the parameters.
+
+(defn foo [x y z]
+  (list x (let [x y]
+            (list x z))))
+
+(defmacro foo [x y z]
+  `(list ~x (let [x# ~y]
+              (list x# ~z))))
+
