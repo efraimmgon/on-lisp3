9c4eb99d5ec2d543f4ea60d68873dc4e56731efd
efraimmgon <efraimmaugusto@gmail.com>
2022-11-06 09:53:57 -0400

    macro argument capture

--- src/on_lisp/chap09.clj
+++ src/on_lisp/chap09.clj
@@ -6,3 +6,24 @@
 ;;; 9
 ;;; Variable Capture
 ;;; ----------------------------------------------------------------------------
+
+;;; ----------------------------------------------------------------------------
+;;; 9.1 Macro Argument Capture
+
+
+(defmacro pfor [[var start stop] & body]
+  `(loop [~var ~start
+          ~'limit ~stop]
+     (when (<= ~var ~'limit)
+       ~@body
+       (recur (inc ~var)
+              ~'limit))))
+
+#_(utils/mac
+   (pfor [x 1 5] (pr x)))
+
+; this will loop forever
+#_(pfor [limit 1 5] (pr limit))
+
+;;; ----------------------------------------------------------------------------
+;;; 9.2 Free Symbol Capture
