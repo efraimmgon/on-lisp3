e77dbe0ce0f8547c226f05e5acb82b3892438556
efraimmgon <efraimmaugusto@gmail.com>
2022-11-07 16:24:22 -0400

    number of evaluations

--- src/on_lisp/chap10.clj
+++ src/on_lisp/chap10.clj
@@ -6,3 +6,28 @@
 ;;; 10
 ;;; Other Macro Pitfalls
 ;;; ----------------------------------------------------------------------------
+
+;;; ----------------------------------------------------------------------------
+;;; 10.1 Number of Evaluations
+
+;; A correct version:
+(defmacro our-for [[var start stop] & body]
+  `(loop [~var ~start
+          stop# ~stop]
+     (when-not (> ~var stop#)
+       (recur (inc ~var) stop#))))
+
+;; Subject to multiple evaluations:
+#_(defmacro our-for [[var start stop] & body]
+    `(loop [~var ~start]
+       (when-not (> ~var ~stop)
+         (recur (inc ~var)))))
+
+;; Incorrect order of evaluation:
+#_(defmacro our-for [[var start stop] & body]
+    `(loop [stop# ~stop
+            ~var ~start]
+       (when-not (> ~var stop#)
+         (recur (inc ~var) stop#))))
+
+
