1555b4f2738e852822ca58c7eeabd050bd6898e6
efraimmgon <efraimmaugusto@gmail.com>
2022-11-16 16:49:14 -0400

    anaphoric variants of operators

---
+++ src/on_lisp/chap14.clj
@@ -0,0 +1,58 @@
+(ns on-lisp.chap14
+  (:require
+   [on-lisp.utils :as utils]))
+
+;;; ----------------------------------------------------------------------------
+;;; 14
+;;; Anaphoric Macros
+;;; ----------------------------------------------------------------------------
+
+;;; ----------------------------------------------------------------------------
+;;; 14.1 Anaphoric Variants
+
+(defmacro aif
+  "Anaphoric if. `it` is used to bind the result of `test-form`."
+  [test-form then-form & [else-form]]
+  `(let [~'it ~test-form]
+     (if ~'it ~then-form ~else-form)))
+
+#_(utils/mac (aif (inc 1) it false))
+
+(defmacro awhen
+  "Anaphoric when. `it` is used to bind the result of `test-form`."
+  [test-form & body]
+  `(let [~'it ~test-form]
+     (when ~'it
+       ~@body)))
+
+(defmacro awhile
+  "Anaphoric while `it` is used to bind the result of `test-form`."
+  [test-form & body]
+  `(loop [~'it ~test-form]
+     (when ~'it
+       ~@body
+       (recur ~test-form))))
+
+(defmacro aand
+  "Anaphoric and. During the evaluation of each of its arguments, `it` will 
+   be bound to the value returned by the previous argument."
+  [& args]
+  (cond (empty? args) true
+        (empty? (next args)) (first args)
+        :else `(aif ~(first args)
+                    (aand ~@(next args)))))
+
+; In practice, `aand` tends to be used in programs which make conditional 
+; queries, as in:
+; (aand (owner x) (address it) (town it))
+
+(defmacro acond
+  ""
+  [& clauses]
+  (when (seq clauses)
+    (let [cl1 (take 2 clauses)]
+      `(let [sym# ~(first cl1)]
+         (if sym#
+           (let [~'it sym#]
+             (last cl1))
+           (acond ~@(drop 2 clauses)))))))
